5226dfb34defcda5d0030e3de32102588a4ff72c
Stefan Haun <stefan.haun@ovgu.de>
2013-03-21 14:09:54 +0100

    SQL-Proxy for Note

---
+++ src/main/java/com/penguineering/virtualnotebook/store/sql/NoteSqlPoxy.java
@@ -0,0 +1,110 @@
+package com.penguineering.virtualnotebook.store.sql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import javax.sql.DataSource;
+
+import com.penguineering.virtualnotebook.model.Bullet;
+import com.penguineering.virtualnotebook.model.Note;
+import com.penguineering.virtualnotebook.model.builder.NoteBuilder;
+import com.penguineering.virtualnotebook.store.bean.BeanModelFactory;
+
+public class NoteSqlPoxy extends SqlProxy<Note> implements Note {
+
+	public NoteSqlPoxy(int id, DataSource ds) {
+		super(id, ds);
+	}
+
+	@Override
+	public int getId() {
+		assertDataLoaded();
+		return getData().getId();
+	}
+
+	@Override
+	public Date getTimestamp() {
+		assertDataLoaded();
+		return getData().getTimestamp();
+	}
+
+	@Override
+	public String getType() {
+		assertDataLoaded();
+		return getData().getType();
+	}
+
+	@Override
+	public String getTitle() {
+		assertDataLoaded();
+		return getData().getTitle();
+	}
+
+	@Override
+	public List<Bullet> getBullets() {
+		assertDataLoaded();
+		return getData().getBullets();
+	}
+
+	@Override
+	protected Note load(int id, DataSource ds) throws SQLException,
+			NoSuchElementException {
+		Connection con = ds.getConnection();
+
+		final NoteBuilder note = new NoteBuilder();
+
+		try {
+			loadCore(con, id, note);
+			loadBullets(ds, con, id, note);
+		} finally {
+			con.close();
+		}
+
+		return BeanModelFactory.INSTANCE.createNote(note);
+	}
+
+	private void loadCore(Connection con, int id, NoteBuilder builder)
+			throws SQLException {
+		// TODO das ist eigentlich ein JOIN
+		final PreparedStatement ps = con
+				.prepareStatement("SELECT entries.timestamp, notes.title "
+						+ "FROM entries, notes "
+						+ "WHERE entries.id=? AND notes.entry_id=entries.id AND entries.type='note'");
+		ps.setInt(1, id);
+
+		final ResultSet rs = ps.executeQuery();
+
+		if (!rs.next())
+			throw new NoSuchElementException("A bullet with id " + id
+					+ " could not be found!");
+
+		builder.setId(id);
+		builder.setTimestamp(new Date(rs.getLong(1)));
+		builder.setTitle(rs.getString(2));
+
+		rs.close();
+		ps.close();
+	}
+
+	private void loadBullets(DataSource ds, Connection con, int id,
+			NoteBuilder builder) throws SQLException {
+		final PreparedStatement ps = con.prepareStatement("SELECT id "
+				+ "FROM bullets WHERE note_id=? AND parent_id IS NULL");
+		ps.setInt(1, id);
+
+		final ResultSet rs = ps.executeQuery();
+
+		while (rs.next())
+			builder.addBullet(new BulletSqlProxy(rs.getInt(1), ds));
+
+		rs.close();
+		ps.close();
+
+	}
+
+}
